26e938d14e0c20df6c62aef0a34d0897e4840650
Florian <flo.aurousseau@gmail.com>
2021-02-16 15:47:16 +0100

    Fin de séance

--- src/ClientsManager.php
+++ src/ClientsManager.php
@@ -2,6 +2,8 @@
 
 use Slim\App;
 
+class Client {}
+
 class ClientsManager extends PDO {
     
     function __construct(App $app) {
@@ -25,31 +27,108 @@ class ClientsManager extends PDO {
         }
     }
 
+    function wipe()
+    {
+        $queryA = $this->prepare("DELETE FROM client");
+
+        $queryA->execute();
+
+        $query = $this->prepare("INSERT INTO client (id, prenom, nom, telephone)
+        VALUES  (1,'Bryan','Binet','0624563146')
+              , (2,'Corentin','Derimay','0645789425') 
+              , (3,'Florian','Aurousseau','0798463249') 
+              , (4,'Robbin','Freville','0654234578') 
+              , (5,'Robin','Zmuda','0234578965') 
+              , (6,'Jimmy','Trespalle','032145785') 
+              , (7,'Esteban','Loubatiere','0789453246') 
+              , (8,'Theophile','Hesters','0632489756') 
+              , (9,'Theo','Bertrand','0645289437') 
+              , (10,'Antoine','Sanson','0642789425') 
+              , (11,'Axel','Dardat','0645789425') 
+              , (12,'Mathias','Morel','0789425368') 
+              , (13,'Leo','Laurent','0147532259') 
+              , (14,'Yann','Claudon','0457812249') 
+              , (15,'Nicolas','Breugnot','0314758944') 
+              , (16,'Maxime','Michel','0611442579') 
+              , (17,'Romain','Aoulini','0245789543') 
+              , (18,'Nicolas','Thomas','0642155796') 
+              , (19,'Dimitry','Vincent','0642115789') 
+              , (20,'Joris','Texier','0245778966') ");
+
+        $query->execute();
+    }
+
     public function getClients()
     {
-        $query = $this->prepare('SELECT * FROM client');
-        $query->execute();
+        try {
+            $query = $this->prepare('SELECT * FROM client');
+            $query->execute();
+        }
+        catch(PDOException $e) {
+            return "{message: ".$e->getMessage()."}";
+        }
 
-        return $query->fetchAll(PDO::FETCH_OBJ);
+        return json_encode($query->fetchAll(PDO::FETCH_OBJ));
     }
 
     public function getClient($id)
     {
-        
+        try {
+            $query = $this->prepare('SELECT * FROM client WHERE id='.$id);
+            $query->execute();
+        }
+        catch(PDOException $e) {
+            return "{message: ".$e->getMessage()."}";
+        }
+
+        return json_encode($query->fetch(PDO::FETCH_OBJ));
     }
 
     public function deleteClient($id)
     {
-        
+        $this->wipe();
+
+        try {
+            $query = $this->prepare('DELETE FROM client WHERE id='.$id);
+            $query->execute();
+        }
+        catch(PDOException $e) {
+            return "{message: ".$e->getMessage()."}";
+        }
+
+        return '{message: "success"}';
     }
 
-    public function addClient($client)
+    public function addClient(array $client)
     {
+        $this->wipe();
 
+        try {
+            $query = $this->prepare('INSERT INTO CLIENT VALUES 
+            ('.$client['id'].',
+            '.$client['nom'].',
+            '.$client['prenom'].',
+            '.$client['telephone'].
+            ')');
+    
+            $query->execute();
+    
+            return json_encode($query->fetchAll(PDO::FETCH_OBJ));
+        }
+        catch(PDOException $e) {
+            return "{message: ".$e->getMessage()."}";
+        }
     }
 
-    public function alterClient($client)
+    public function alterClient(array $client)
     {
+        $query = $this->prepare('UPDATE CLIENT 
+        SET nom='.$client['nom'].',
+            prenom='.$client['prenom'].',
+            telephone='.$client['telephone'].
+        'WHERE id='.$client['id']);
+        $query->execute();
 
+        return $query->fetchAll(PDO::FETCH_OBJ);
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -13,7 +13,45 @@ return function (App $app) {
     $app->get('/clients', function (Request $request, Response $response, array $args) use ($clientManager) {
         
         $clients = $clientManager->getClients();
-        $response->getBody()->write(json_encode($clients));
+        $response->getBody()->write($clients);
+
+        return $response->withHeader('Content-Type', 'application/json');
+    });
+
+    $app->get('/client/{id}', function (Request $request, Response $response, array $args) use ($clientManager) {
+
+        $client = $clientManager->getClient((int)$args['id']);
+        $response->getBody()->write($client);
+
+        return $response->withHeader('Content-Type', 'application/json');
+    });
+
+    $app->post('/addClient', function (Request $request, Response $response, array $args) use ($clientManager) {
+        
+        //$client = 
+
+        /*$client = array(
+            "id" => $args['id'],
+            "nom" => $args['nom'],
+            "prenom" => $args['prenom'],
+            "telephone" => $args['telephone']
+        );*/
+
+        $res = $clientManager->addClient($client);
+        $response->getBody()->write($res);
+
+        return $response->withHeader('Content-Type', 'application/json');
+    });
+
+
+
+
+
+
+    $app->get('/deleteClient/{id}', function (Request $request, Response $response, array $args) use ($clientManager) {
+        
+        $res = $clientManager->deleteClient($args['id']);
+        $response->getBody()->write($res);
 
         return $response->withHeader('Content-Type', 'application/json');
     });
